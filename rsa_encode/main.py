b79e2e8a06e0e3dfaf4620b6c7aa0e2c202b7618
mandu <91869877+GonManDoo@users.noreply.github.com>
2021-12-07 23:15:53 +0900

    Update main.py

--- rsa_encode/main.py
+++ rsa_encode/main.py
@@ -7,8 +7,6 @@ from rsa import rsa_encode
 from rsa import key_make
 from rsa import rsa_decode
 
-
-# plain_text(평문)의 아스키 값을 encoded_text 로 전달
 plain_text = file_copy_to_list_encode("C:\\test.txt")
 prime_num_list = euler_of_sieve(10, 80)
 key_make(prime_num_list)
@@ -16,8 +14,7 @@ key_make(prime_num_list)
 encoded_text = rsa_encode(plain_text)
 list_copy_to_file_encode("C:\\encoded.txt", encoded_text)
 
-print("encoded_text:", encoded_text)
 test123 = file_copy_to_list_decode("C:\\encoded.txt")
-
-decoded_text = rsa_decode(encoded_text)
+print(test123)
+decoded_text = rsa_decode(test123)
 list_copy_to_file_decode("C:\\decoded.txt", decoded_text)
